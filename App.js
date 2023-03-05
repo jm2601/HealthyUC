d4c99bd1481fe2813e18556a8b0be4b0a5307b5d
Javier <javier.miranda4420@gmail.com>
2023-03-04 17:29:32 -0800

    moved createAccount to Components and added text fields to create account

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import LoginScreen from './screens/LoginScreen';
-
+import CreateAccount from './components/createAccount';
 
 //main function
 export default function App() {
   return (
     <View style={styles.container}>
       <LoginScreen />
+      <CreateAccount/>
     </View>
   );
 }
