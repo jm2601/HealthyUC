59df9b1adb8e329f418101b3073952b22ecf31d2
Javier <javier.miranda4420@gmail.com>
2023-03-05 02:22:52 -0800

    created folder src and added nav and screens

---
+++ src/screens/createAccount.js
@@ -0,0 +1,58 @@
+import React, { useState } from 'react';
+import { View, TextInput, Button, StyleSheet } from 'react-native';
+
+const CreateAccount = () => {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleCreateAccount = () => {
+    // Perform account creation logic here
+    console.log(`Creating account with name: ${name} email: ${email} and password: ${password}`);
+  };
+
+  return (
+    <View style={styles.container}>
+      <TextInput
+        style={styles.input}
+        placeholder="Name"
+        onChangeText={setName}
+        value={name}
+      />
+
+      <TextInput
+        style={styles.input}
+        placeholder="Email"
+        onChangeText={setEmail}
+        value={email}
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Password"
+        secureTextEntry={true}
+        onChangeText={setPassword}
+        value={password}
+      />
+      <Button title="Create Account" onPress={handleCreateAccount} />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderColor: '#ccc',
+    padding: 10,
+    marginVertical: 10,
+    borderRadius: 5,
+    width: '100%',
+  },
+});
+
+export default CreateAccount;
