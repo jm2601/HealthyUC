aad3f1ba5ebfc29b0808bd773ff85f8934fd966d
Javier <javier.miranda4420@gmail.com>
2023-03-05 02:22:12 -0800

    added stack navigator

--- App.js
+++ App.js
@@ -1,21 +1,7 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import LoginScreen from './screens/LoginScreen';
-//import CreateAccount from './components/createAccount';
-//add <CreateAccount/> under LoginScreen later 
+import React from 'react'
 
-//main function
+import MainStackNavigator from './src/navigation/MainStackNavigator'
+
 export default function App() {
-  return (
-    //<View style={styles.container}>
-      <LoginScreen />
-    //</View>
-  );
+  return <MainStackNavigator />
 }
-//styles
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-  },
-});
